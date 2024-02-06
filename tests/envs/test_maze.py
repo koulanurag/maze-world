7731f88fdc9d771b569b173960b6a2748518efe4
Anurag Koul <koulanurag@gmail.com>
2024-02-06 17:48:11 -0500

    tests: reset test for initial agent and target pos.

--- tests/envs/test_maze.py
+++ tests/envs/test_maze.py
@@ -2,12 +2,27 @@ import gymnasium as gym
 import pytest
 
 
-@pytest.mark.parametrize('env_name',
-                         ["RandomMaze-11x11-v0"])
+@pytest.mark.parametrize(
+    "env_name",
+    [
+        "RandomMaze-11x11-v0",
+        "RandomMaze-21x21-v0",
+        "RandomMaze-31x31-v0",
+        "RandomMaze-101x101-v0",
+    ],
+)
 def test_init(env_name):
-    env = gym.make(f'maze_world:{env_name}')
+    env = gym.make(f"maze_world:{env_name}")
     observation, info = env.reset()
-    assert env._prev_agent_location is None
+
+    assert all(observation["agent"] == [1, 1]), "initial agent position is not correct"
+    assert all(
+        observation["target"]
+        == [
+            env.unwrapped.maze_width - 2,
+            env.unwrapped.maze_height - 2,
+        ]
+    ), "target position is not correct"
 
     # rollout
     for step_i in range(5):
@@ -16,4 +31,14 @@ def test_init(env_name):
         if terminated or truncated:
             break
     observation, info = env.reset()
-    assert env._prev_agent_location is None
+
+    assert all(
+        observation["agent"] == [1, 1]
+    ), "initial agent position is not correct, after episode run and reset"
+    assert all(
+        observation["target"]
+        == [
+            env.unwrapped.maze_width - 2,
+            env.unwrapped.maze_height - 2,
+        ]
+    ), "target position is not correct, after episode run and reset"
